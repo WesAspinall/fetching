b49dcd1a5d53ce2e74723e13865ff196739ae308
wesaspinall <wes@wesaspinall.com>
2017-12-29 16:12:42 -0500

    $.ajax()...displayed placeholder imgs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 {
   const ROOT_URL = 'https://jsonplaceholder.typicode.com';
   const USERS = '/users';
+  const PHOTOS = '/photos';
 
-  const el = document.querySelector("#root");
+  const fetchEl = document.querySelector("#fetch");
+  const jqAjaxEl = $("#jq");
+
   const users = [];
 
-
+//fetch api
   function fetchData(){
     fetch(ROOT_URL+USERS)
     .then((res) => res.json())
@@ -24,11 +27,30 @@
       `
       }).join('');
 
-      el.innerHTML = html;
+      fetchEl.innerHTML = html;
     });
   }
-  
+
+//$.ajax
+  function jqAjax() {
+    $.ajax({
+      url: ROOT_URL+PHOTOS,
+      method: 'GET'
+    })
+    .then((res) => {
+      sliced = res.slice(0,6);
+      return sliced
+    })
+    .then((photos) => {
+      photos.forEach((photo) => {
+        $(".jq-loading").remove();
+        jqAjaxEl.append(`<img src='${photo.url}'>`);
+      })
+    });
+  }
+
   fetchData();
+  jqAjax();
 }
 
    
