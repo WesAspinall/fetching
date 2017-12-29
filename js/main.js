ef29522c475891ae8851660c268e20c25feeeda9
wesaspinall <wes@wesaspinall.com>
2017-12-29 01:01:04 -0500

    got data from jsonplaceholder and put it on page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 {
-  console.log('hello from main.js');
+  const ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  const USERS = '/users';
+
+  const el = document.querySelector("#root");
+  const users = [];
+
+
+  function fetchData(){
+    fetch(ROOT_URL+USERS)
+    .then((res) => res.json())
+    .then((data) => {
+      users.push(...data);
+      html = users.map((user) => {
+        return `
+          <ul>
+            <li>${user.name}</li>
+            <li>${user.username}</li>
+            <li>${user.email}</li>
+            <li>${user.website}</li>
+            <li>${user.phone}</li>
+          </ul>
+      `
+      }).join('');
+
+      el.innerHTML = html;
+    });
+  }
+  
+  fetchData();
 }
+
+   
